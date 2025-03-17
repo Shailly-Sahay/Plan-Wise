db854d7e2f945443ea50370d3be2f3dc95ed4164
Shailly-Sahay <shaillysahayy@gmail.com>
2025-03-17 19:32:25 +0530

    ch

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -1,12 +1,10 @@
-module.exports = {
-  apps: [
-    {
-      name: "Plan Wise",
-      scripts: "npm",
-      args: "run dev",
-      env: {
-        NODE_ENV: "development",
-      },
+export const apps = [
+  {
+    name: "Plan Wise",
+    scripts: "npm",
+    args: "run dev",
+    env: {
+      NODE_ENV: "development",
     },
-  ],
-};
+  },
+];
